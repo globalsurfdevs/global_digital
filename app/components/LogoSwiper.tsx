7dd30cbdbc6082301e9bc4c4b6a7ca498ff3e0f5
rahees <raheez1995india@gmail.com>
2024-12-05 12:12:36 +0530

    slider issues

--- app/components/LogoSwiper.tsx
+++ app/components/LogoSwiper.tsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
 import "swiper/css";
 import { logos } from "@/public/assets/assets";
 import { inherits } from "util";
@@ -10,10 +11,27 @@ const LogoSwiper = () => {
   console.log(logos);
   return (
     <div className="bg-bglight py-3 overflow-hidden">
-      <div className="container ">
-        <Swiper className="overflow-inherit" 
+      <div className="container !overflow-visible">
+        <Swiper
+          className="!overflow-visible"
+          modules={[Autoplay]}
           spaceBetween={50}
           slidesPerView={3}
+          speed={3000} // Smooth transition speed
+          autoplay={{
+            delay: 0, // No delay between slides
+            disableOnInteraction: false,
+            reverseDirection: false, // Slide in one direction
+            stopOnLastSlide: false, // Continuous scrolling
+          }}
+          loop={true}
+          freeMode={{
+            enabled: true,
+            momentum: false,
+          }}
+          simulateTouch={false}
+          allowTouchMove={false}
+          watchSlidesProgress={true}
           breakpoints={{
             640: {
               slidesPerView: 3,
