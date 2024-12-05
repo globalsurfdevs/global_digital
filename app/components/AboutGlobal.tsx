7a2a25bdb7a7614e98bc31457a4170e57a544dc1
rahees <raheez1995india@gmail.com>
2024-12-05 11:28:24 +0530

    about button style change

--- app/components/AboutGlobal.tsx
+++ app/components/AboutGlobal.tsx
@@ -14,10 +14,10 @@ const AboutGlobal = () => {
           after:absolute after:bottom-0 after:right-0 after:h-[1px] after:bg-orange-500 after:w-full after:transition-all after:duration-300 after:ease-in-out hover:after:w-0 z-1
         ">
           <h5 className="text-sm font-bold group-hover:text-primary ease-in-out duration-200">ABOUT GS.DIGITAL</h5>
-          <svg width="10" height="10" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" className="group-hover:scale-125 ease-in-out duration-200">
+          <svg width="10" height="10" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" className="group-hover:scale-125 ease-in-out duration-200 ">
             <g clipPath="url(#clip0_65_58)">
-              <path d="M18.7892 1.2749L0.699219 19.0149" stroke="#E53F30" strokeWidth="3" strokeMiterlimit="10" />
-              <path d="M0.699219 1.2749H18.7892V18.6649" stroke="#E53F30" strokeWidth="3" strokeMiterlimit="10" />
+              <path d="M18.7892 1.2749L0.699219 19.0149" stroke="#E53F30" strokeWidth="3" strokeMiterlimit="10" className="group-hover:stroke-black" />
+              <path d="M0.699219 1.2749H18.7892V18.6649" stroke="#E53F30" strokeWidth="3" strokeMiterlimit="10" className="group-hover:stroke-black" />
             </g>
             <defs>
               <clipPath id="clip0_65_58">
