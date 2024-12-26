7b936fe722df23620c606eb32af35654310e4de5
sreemayispraseed <sreemayi99@gmail.com>
2024-12-26 12:53:19 +0530

    home page changes updated

--- app/components/HomePage/OurServices.tsx
+++ app/components/HomePage/OurServices.tsx
@@ -1,50 +1,47 @@
-
 "use client";
-import React, { useEffect, useRef, useState } from "react"
+import React, { useEffect, useRef, useState } from "react";
 import { motion } from "framer-motion";
 import { services } from "../../data/services";
-import {Lexend} from "next/font/google";
+import { Lexend } from "next/font/google";
 import Link from "next/link";
-const lexend = Lexend({subsets: ['latin'] ,weight:["300","400","500","600","700"] });
+const lexend = Lexend({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+});
 
 const OurServices = () => {
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
 
+  // Ref for the next container (HTMLDivElement type)
+  const nextContainerRef = useRef<HTMLDivElement | null>(null);
+  const [divheight, setdivheight] = useState("100%");
 
-    const [isSmallScreen, setIsSmallScreen] = useState(false);
+  useEffect(() => {
+    const updatedivheight = () => {
+      if (nextContainerRef.current) {
+        // Get the bounding rectangle of the next container
+        const containerRect = nextContainerRef.current.getBoundingClientRect();
 
+        const totalHeight = containerRect.height;
 
-
-    // Ref for the next container (HTMLDivElement type)
-    const nextContainerRef = useRef<HTMLDivElement | null>(null);
-    const [divheight, setdivheight] = useState("100%");
-
-    useEffect(() => {
-      const updatedivheight = () => {
-        if (nextContainerRef.current) {
-          // Get the bounding rectangle of the next container
-          const containerRect = nextContainerRef.current.getBoundingClientRect();
-
-
-            const totalHeight = containerRect.height
-
-          setdivheight(`${totalHeight}px`);
-        }
-      };
-
-      if (window.innerWidth > 768) {
-        // Initial height calculation
-        updatedivheight();
-
-        // Recalculate on window resize
-        window.addEventListener("resize", updatedivheight);
+        setdivheight(`${totalHeight}px`);
       }
-      // Cleanup event listener on unmount
-      return () => {
-        window.removeEventListener("resize", updatedivheight);
-      };
-    }, []); // Empty dependency array ensures this runs once on mount
+    };
 
- const checkWidth = () => {
+    if (window.innerWidth > 768) {
+      // Initial height calculation
+      updatedivheight();
+
+      // Recalculate on window resize
+      window.addEventListener("resize", updatedivheight);
+    }
+    // Cleanup event listener on unmount
+    return () => {
+      window.removeEventListener("resize", updatedivheight);
+    };
+  }, []); // Empty dependency array ensures this runs once on mount
+
+  const checkWidth = () => {
     if (window.innerWidth > 768) {
       setIsSmallScreen(true);
     } else {
@@ -65,97 +62,116 @@ const OurServices = () => {
     <>
       {/* Section Heading */}
       <div className="container mx-auto ">
-    <motion.div
-        initial="hidden"
-                whileInView="visible"
-                viewport={{ once: true, amount: 0.3 }} // Trigger animation once when 50% visible
-                variants={{
-                  hidden: { opacity: 0, y: 50 }, // Start below and invisible
-                  visible: { opacity: 1, y: 0, transition: { duration: 1, ease: "easeOut" } }, // Slide up and fade in
-                }}
-      >
-      <div className="lg:mb-[89px] mb-5 flex gap-2 items-center">
-        <h2 className="text-30 leading-[1.5] font-[400]">OUR SERVICES</h2>
-        <div className="bg-primary lg:size-5 size-3 md:size-4"></div>
-      </div>
-
-        </motion.div>
-        </div>
-    <div className="container mx-auto px-4 py-8 flex flex-col gap-5 xl:gap-24">
-
-      {/* Services */}
-      {services.map((service) => (
         <motion.div
-          className="grid grid-cols-1 md:grid-cols-2 gap-8 xl:gap-[88px] srv-item overflow-hidden ref-ht"
-          ref={nextContainerRef}
-        key={service.id}
-        initial="hidden"
-                whileInView="visible"
-                viewport={{ once: true, amount: 0 }} // Trigger animation once when 50% visible
-                variants={{
-                  hidden: { opacity: 0, y: 50 }, // Start below and invisible
-                  visible: { opacity: 1, y: 0, transition: { duration: 1, ease: "easeOut" } }, // Slide up and fade in
-                }}
-      >
-        {/* Service Image */}
-          <div   style={{ height: divheight }}
-          className={`${isSmallScreen ? "h-full border-b-gray-400 md:order-2 srv-im flex justify-end targ-ht" : ""} custom-class`}>
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.3 }} // Trigger animation once when 50% visible
+          variants={{
+            hidden: { opacity: 0, y: 50 }, // Start below and invisible
+            visible: {
+              opacity: 1,
+              y: 0,
+              transition: { duration: 1, ease: "easeOut" },
+            }, // Slide up and fade in
+          }}
+        >
+          <div className="mb-5 flex items-center gap-2 lg:mb-[89px]">
+            <h2 className="text-30 font-[400] leading-[1.5]">OUR SERVICES</h2>
+            <div className="size-3 bg-primary md:size-4 lg:size-5"></div>
+          </div>
+        </motion.div>
+      </div>
+      <div className="container mx-auto flex flex-col gap-5 px-4 py-8 xl:gap-24">
+        {/* Services */}
+        {services.map((service) => (
           <motion.div
-            className="h-full overflow-hidden"
+            className="srv-item ref-ht grid grid-cols-1 gap-8 overflow-hidden md:grid-cols-2 xl:gap-[88px]"
+            ref={nextContainerRef}
+            key={service.id}
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0 }} // Trigger animation once when 50% visible
             variants={{
-              hidden: { y: 50, opacity: 0 }, // Start below and fade in
-              visible: { y: 0, opacity: 1, transition: { duration: 1, delay: 0.2 } },
+              hidden: { opacity: 0, y: 50 }, // Start below and invisible
+              visible: {
+                opacity: 1,
+                y: 0,
+                transition: { duration: 1, ease: "easeOut" },
+              }, // Slide up and fade in
             }}
           >
-            <motion.img
-              src={service.image}
-              alt={service.title}
-              className="h-full object-cover"
-              initial={{ scale: 1 }} // Initial scale
-              whileInView={{ scale: 1.1 }} // Zoom effect
-              viewport={{ once: false, amount: 0.5 }}
-              transition={{ duration: 0.6, ease: "easeInOut" }}
-            />
-          </motion.div>
-        </div>
-
-          {/* Service Details */}
-          <motion.div style={{ height: divheight  }}
-                className={`${isSmallScreen ? "flex justify-between flex-col border-b pb-5 lg:pb-2 targ-ht" : ""} custom-class`}
+            {/* Service Image */}
+            <div
+              style={{ height: divheight }}
+              className={`${isSmallScreen ? "srv-im targ-ht flex h-full justify-end border-b-gray-400 md:order-2" : ""} custom-class`}
+            >
+              <motion.div
+                className="h-full overflow-hidden"
+                variants={{
+                  hidden: { y: 50, opacity: 0 }, // Start below and fade in
+                  visible: {
+                    y: 0,
+                    opacity: 1,
+                    transition: { duration: 1, delay: 0.2 },
+                  },
+                }}
               >
-                {/* Content Block */}
-            <div className="flex flex-col gap-3 cntntblc">
-              <div className="relative group">
-                <h3 className="title-65 max-w-[14ch] group-hover:text-primary transition-all duration-300 ease-in-out">{service.title}</h3>
-                <Link href={service.url} className="absolute w-full h-full top-0 z-[1]  "></Link>
-              </div>
-                  <div className="flex flex-col gap-2 lg:gap-7">
-                    <p className={`mb-2 text-font25 leading-lh1p4 ${lexend.className} font-light`}>
-                      {service.description}
-                    </p>
-                    <div className="flex flex-wrap gap-2 mb-5 xl:mb-[4em] srvbt">
-                      {service.buttonTexts.map((item, index) => (
-                        <button
-                          className="px-3 py-2 border text-gray-500 rounded-full text-sm font-[500] hover:border-primary hover:text-black ease-in duration-200"
-                          key={index}
-                        >
-                          {item.title}
-                        </button>
-                      ))}
-                    </div>
+                <motion.img
+                  src={service.image}
+                  alt={service.title}
+                  className="h-full object-cover"
+                  initial={{ scale: 1 }} // Initial scale
+                  whileInView={{ scale: 1.1 }} // Zoom effect
+                  viewport={{ once: false, amount: 0.5 }}
+                  transition={{ duration: 0.6, ease: "easeInOut" }}
+                />
+              </motion.div>
+            </div>
+
+            {/* Service Details */}
+            <motion.div
+              style={{ height: divheight }}
+              className={`${isSmallScreen ? "targ-ht flex flex-col justify-between border-b pb-5 lg:pb-2" : ""} custom-class`}
+            >
+              {/* Content Block */}
+              <div className="cntntblc flex flex-col gap-3">
+                <div className="group relative">
+                  <h3 className="title-65 max-w-[14ch] transition-all duration-300 ease-in-out group-hover:text-primary">
+                    {service.title}
+                  </h3>
+                  <Link
+                    href={service.url}
+                    className="absolute top-0 z-[1] h-full w-full  "
+                  ></Link>
+                </div>
+                <div className="flex flex-col gap-2 lg:gap-7">
+                  <p
+                    className={`mb-2 text-font25 leading-lh1p4 ${lexend.className} font-light text-[#000]`}
+                  >
+                    {service.description}
+                  </p>
+                  <div className="srvbt mb-5 flex flex-wrap gap-2 xl:mb-[4em]">
+                    {service.buttonTexts.map((item, index) => (
+                      <button
+                        className="rounded-full border px-4 py-3 text-font19 font-[500] text-gray-500 duration-200 ease-in hover:border-primary hover:text-black"
+                        key={index}
+                      >
+                        {item.title}
+                      </button>
+                    ))}
                   </div>
                 </div>
+              </div>
 
-                  {/* Service ID */}
-                  <div className="flex mb-3">
-                    <span>0{service.id}</span>
-                  </div>
-              </motion.div>
-
-        </motion.div>
-      ))}
+              {/* Service ID */}
+              <div className="mb-3 flex">
+                <span className="text-gray-500">0{service.id}</span>
+              </div>
+            </motion.div>
+          </motion.div>
+        ))}
       </div>
-      </>
+    </>
   );
 };
 
--- app/components/HomePage/SuccessStories.tsx
+++ app/components/HomePage/SuccessStories.tsx
@@ -1,102 +1,133 @@
-"use client"
+"use client";
 
 import React, { useRef } from "react";
 import { stories } from "../../data/stories";
-import {Lexend} from "next/font/google";
+import { Lexend } from "next/font/google";
 
-
-import { motion } from 'framer-motion';
-const lexend = Lexend({subsets: ['latin'] ,weight:["300","400","500","600","700"] });
+import { motion } from "framer-motion";
+const lexend = Lexend({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+});
 
 const SuccessStories = () => {
+  const ref = useRef(null);
 
-const ref = useRef(null)
+  // useEffect(() => {
+  //   if (isInView) {
+  //     const startAnimations = () => {
+  //       stories.forEach((story, index) => {
+  //         animate(
+  //           0,
+  //           story.count,
+  //           {
+  //             duration: 2, // Duration in seconds
+  //             onUpdate: (value) => {
+  //               setAnimatedValues((prev) => {
+  //                 const updated = [...prev];
+  //                 updated[index] = Math.round(value); // Update with rounded value
+  //                 return updated;
+  //               });
+  //             },
+  //           }
+  //         );
+  //       });
+  //     };
 
-
-
-
-
-
-// useEffect(() => {
-//   if (isInView) {
-//     const startAnimations = () => {
-//       stories.forEach((story, index) => {
-//         animate(
-//           0,
-//           story.count,
-//           {
-//             duration: 2, // Duration in seconds
-//             onUpdate: (value) => {
-//               setAnimatedValues((prev) => {
-//                 const updated = [...prev];
-//                 updated[index] = Math.round(value); // Update with rounded value
-//                 return updated;
-//               });
-//             },
-//           }
-//         );
-//       });
-//     };
-
-//     startAnimations();
-//   }
-// }, [isInView]);
-
+  //     startAnimations();
+  //   }
+  // }, [isInView]);
 
   return (
-    <div className="container px-4 mx-auto">
-
-      <div className="pt-6 lg:pt-[70px]  pb-[50px] lg:pb-[150px]  flex flex-col gap-6 lg:gap-[50px]">
-      <motion.div
+    <div className="container mx-auto px-4">
+      <div className="flex flex-col  gap-6 pb-[50px]  pt-6 lg:gap-[50px] lg:pb-[150px] lg:pt-[70px]">
+        <motion.div
           initial="hidden"
-                  whileInView="visible"
-                  viewport={{ once: true, amount: 0.3 }} // Trigger animation once when 50% visible
-                  variants={{
-                    hidden: { opacity: 0, y: 50 }, // Start below and invisible
-                    visible: { opacity: 1, y: 0, transition: { duration: 1, ease: "easeOut" } }, // Slide up and fade in
-                  }}
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.3 }} // Trigger animation once when 50% visible
+          variants={{
+            hidden: { opacity: 0, y: 50 }, // Start below and invisible
+            visible: {
+              opacity: 1,
+              y: 0,
+              transition: { duration: 1, ease: "easeOut" },
+            }, // Slide up and fade in
+          }}
         >
           <h2 className="title-65">Discover Our Success Stories</h2>
-
         </motion.div>
         <motion.div
           initial="hidden"
-                  whileInView="visible"
-                  viewport={{ once: true, amount: 0.3 }} // Trigger animation once when 50% visible
-                  variants={{
-                    hidden: { opacity: 0, y: 50 }, // Start below and invisible
-                    visible: { opacity: 1, y: 0, transition: { duration: 1, ease: "easeOut" } }, // Slide up and fade in
-                  }}
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.3 }} // Trigger animation once when 50% visible
+          variants={{
+            hidden: { opacity: 0, y: 50 }, // Start below and invisible
+            visible: {
+              opacity: 1,
+              y: 0,
+              transition: { duration: 1, ease: "easeOut" },
+            }, // Slide up and fade in
+          }}
         >
-        <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-8">
-          {stories.map((item, index) => (
-            <div className="bg-black   group text-white hover:cursor-pointer ease-linear duration-300" key={index} ref={ref}>
-              <div className="bg-gray2 py-6 lg:py-12  flex flex-col gap-5 justify-center px-8 md:px-12 group-hover:bg-primary ease-in-out duration-300">
-                <div className="relative h-full flex flex-col justify-between">
-                  <h3 className="text-font30 leading-lh1p66 mb-[14px]">{item.title1}</h3>
-                  <h3 className="text-white text-font65 leading-lh0p76 mb-[28px]">
-                    {/* {index == 0 ? <span>&#8595;</span> : <span>&#8593;</span>}
+          <div className="grid grid-cols-1 gap-8 lg:grid-cols-2 xl:grid-cols-3">
+            {stories.map((item, index) => (
+              <div
+                className="group   bg-black text-white duration-300 ease-linear hover:cursor-pointer"
+                key={index}
+                ref={ref}
+              >
+                <div className="flex flex-col justify-center  gap-5 bg-gray2 px-8 py-6 duration-300 ease-in-out group-hover:bg-primary lg:py-12 xl:px-[3.5rem]">
+                  <div className="relative flex h-full flex-col justify-between">
+                    <h3 className="mb-[14px] text-font30 leading-lh1p66">
+                      {item.title1}
+                    </h3>
+                    <h3 className="mb-[28px] text-font65 leading-lh0p76 text-white">
+                      {/* {index == 0 ? <span>&#8595;</span> : <span>&#8593;</span>}
                     {animatedValues[index] + "%"} */}
-                    {item.count}
-                  </h3>
-                  <h3 className={`text-font25 w-3/4 leading-lh1p4 ${lexend.className}`}>{item.description1}</h3>
-                  <div className="absolute top-0 right-0 opacity-0 group-hover:opacity-100 ease-in-out duration-500">
-                    <svg width="36" height="35" viewBox="0 0 36 35" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M33.8125 1.7998L1.25977 33.7227" stroke="white" strokeWidth="2" strokeMiterlimit="10" />
-                      <path d="M1.25977 1.7998H33.8125V33.0929" stroke="white" strokeWidth="2" strokeMiterlimit="10" />
-                    </svg>
+                      {item.count}
+                    </h3>
+                    <h3
+                      className={`w-3/4 text-font25 leading-lh1p4 ${lexend.className}`}
+                    >
+                      {item.description1}
+                    </h3>
+                    <div className="absolute right-0 top-0 opacity-0 duration-500 ease-in-out group-hover:opacity-100">
+                      <svg
+                        width="36"
+                        height="35"
+                        viewBox="0 0 36 35"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M33.8125 1.7998L1.25977 33.7227"
+                          stroke="white"
+                          strokeWidth="2"
+                          strokeMiterlimit="10"
+                        />
+                        <path
+                          d="M1.25977 1.7998H33.8125V33.0929"
+                          stroke="white"
+                          strokeWidth="2"
+                          strokeMiterlimit="10"
+                        />
+                      </svg>
+                    </div>
                   </div>
                 </div>
+                <div className="flex flex-col justify-center gap-8 px-8 py-12  text-white xl:px-[3.5rem]">
+                  <h3 className="text-font30 leading-lh1p26">{item.title2}</h3>
+                  <p
+                    className={`text-font19 leading-lh1p4 ${lexend.className}`}
+                  >
+                    {item.description2}
+                  </p>
+                </div>
               </div>
-              <div className="text-white flex flex-col justify-center gap-8 py-12  px-8 md:px-12">
-                <h3 className="text-font30 leading-lh1p26">{item.title2}</h3>
-                <p className={`text-font19 leading-lh1p4 ${lexend.className}`}>{item.description2}</p>
-              </div>
-            </div>
-          ))}
-        </div>
+            ))}
+          </div>
         </motion.div>
-        </div>
+      </div>
     </div>
   );
 };
