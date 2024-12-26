80d34c7930ea31beac48d94396363b03a5baa8ab
sreemayispraseed <sreemayi99@gmail.com>
2024-12-26 13:32:21 +0530

    case study issues fixed and testimonials p color changed

--- app/components/CaseStudy/HeroSection.tsx
+++ app/components/CaseStudy/HeroSection.tsx
@@ -1,119 +1,155 @@
-"use client"
-import { motion } from 'framer-motion';
-import { assets } from '@/public/assets/assets'
-import Image from 'next/image'
+"use client";
+import { motion } from "framer-motion";
+import { assets } from "@/public/assets/assets";
+import Image from "next/image";
 
 const HeroSection = () => {
-
   return (
     <>
-      <div className='container mx-auto py-4'>
-      <motion.div
-                    initial="hidden"
-                    whileInView="visible"
-                    viewport={{ once: true, amount: 0.3 }} // Trigger animation once when 50% visible
-                    variants={{
-                      hidden: { opacity: 0, y: 50 }, // Start below and invisible
-                      visible: { opacity: 1, y: 0, transition: { duration: 1, ease: "easeOut" } }, // Slide up and fade in
-                      }}
+      <div className="container mx-auto py-4">
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.3 }} // Trigger animation once when 50% visible
+          variants={{
+            hidden: { opacity: 0, y: 50 }, // Start below and invisible
+            visible: {
+              opacity: 1,
+              y: 0,
+              transition: { duration: 1, ease: "easeOut" },
+            }, // Slide up and fade in
+          }}
         >
-        <div className='lg:grid lg:grid-cols-8 items-center border-b lg:pt-[130px] sm:pt-[50px] pt-[20px] pb-10 flex flex-col gap-2'>
+          <div className="flex flex-col items-center gap-2 border-b pb-10 pt-[20px] sm:pt-[50px] lg:grid lg:grid-cols-8 lg:pt-[130px]">
+            <div className="h-full w-full text-font80 lg:col-span-5 lg:text-left xl:col-span-5">
+              <h1 className="title-80">
+                How Seleo Achieved 10x Revenue Growth
+              </h1>
+            </div>
 
-          <div className='text-font80 lg:col-span-5 xl:col-span-5 lg:text-left w-full h-full'>
-            <h1 className='title-80'>How Seleo Achieved 10x Revenue Growth</h1>
-          </div>
-
-          <div className='lg:col-span-3 xl:col-span-3 flex flex-col gap-5 lg:gap-18 lg:items-end w-full h-full justify-between lg:py-4 pt-4 pb-0'>
-            <Image src={assets.seleoLogo} alt='image' className='w-20 md:w-48'/>
-            <h3 className='text-30 text-gray1'>Refreshing, Sparkling, Uplifting</h3>
-          </div>
-
-        </div>
-        </motion.div>
-
-        <motion.div
-        initial="hidden"
-        whileInView="visible"
-        viewport={{ once: true, amount: 0.3 }} // Trigger animation once when 50% visible
-        variants={{
-          hidden: { opacity: 0, y: 50 }, // Start below and invisible
-          visible: { opacity: 1, y: 0, transition: { duration: 1.3, ease: "easeOut" } }, // Slide up and fade in
-          }}
-              >
-        <div className='lg:py-[142px] py-[50px] flex flex-col lg:w-[70%] gap-2 lg:gap-6'>
-
-          <div className='flex items-center gap-2'>
-            <h4 className='text-30 leading-[1.5]'>STORY</h4>
-            <div className='w-4 h-4 lg:w-5 lg:h-5 bg-primary'></div>
-          </div>
-
-          <div  >
-            <p className='text-gray1 fnt-lexend text-font19'>Seleo, a prominent Middle Eastern beverage brand, sought to expand its digital footprint across the UAE, Kuwait, and Qatar. To achieve this, they partnered with Global Surf to revitalize their online presence and optimize marketing campaigns. The goal was to create a compelling digital experience that resonated with their target audience and drove customer engagement.</p>
-          </div>
-        </div>
-
-        </motion.div>
-
-      </div>
-      <motion.div
-        initial="hidden"
-        whileInView="visible"
-        viewport={{ once: true, amount: 0.3 }} // Trigger animation once when 50% visible
-        variants={{
-          hidden: { opacity: 0, y: 50 }, // Start below and invisible
-          visible: { opacity: 1, y: 0, transition: { duration: 1.3, ease: "easeOut" } }, // Slide up and fade in
-          }}
-              >
-      <div className='bg-bglight w-full flex flex-col  overflow-hidden'>
-
-        <div className='container relative'>
-
-        <motion.div
-        initial="hidden"
-        whileInView="visible"
-        viewport={{ once: true, amount: 0.3 }} // Trigger animation once when 50% visible
-        variants={{
-          hidden: { opacity: 0, y: 50 }, // Start below and invisible
-          visible: { opacity: 1, y: 0, transition: { duration: 1.5, ease: "easeOut" } }, // Slide up and fade in
-          }}
-              >
-          <div className={"flex h-full "}>
-            <div className='flex flex-col  py-[50px] lg:py-[150px]  w-full lg:w-1/4'>
-
-              <div className='border-b border-black flex flex-col pb-[20px] mb-[20px] :lgpb-[40px] lg:mb-[36px]'>
-                <h5 className='text-19 text-gray-400 pb-[5px] fnt-lexend'>Industry</h5>
-                <h4 className='text-30'>Food & Beverage</h4>
-              </div>
-
-              <div className='border-b border-black flex flex-col pb-[20px] mb-[20px] :lgpb-[40px] lg:mb-[36px]'>
-                <h5 className='text-19 text-gray-400 pb-[5px] fnt-lexend'>Country</h5>
-                <h4 className='text-30'>UAE, Kuwait, Qatar</h4>
-              </div>
-
-              <div className='border-black flex flex-col  '>
-                <h5 className='text-19 text-gray-400 pb-[5px] fnt-lexend'>Channels Used</h5>
-                <h4 className='text-30'>Website - Google Ads - META Ads</h4>
-              </div>
-
+            <div className="lg:gap-18 flex h-full w-full flex-col justify-between gap-5 pb-0 pt-4 lg:col-span-3 lg:items-end lg:py-4 xl:col-span-3">
+              <Image
+                src={assets.seleoLogo}
+                alt="image"
+                className="w-20 md:w-48"
+              />
+              <h3 className="text-30 text-gray1">
+                Refreshing, Sparkling, Uplifting
+              </h3>
             </div>
           </div>
-          </motion.div>
+        </motion.div>
 
-          {<div className='bg-black h-full absolute top-0 right-[-145px] left-1/3 hidden lg:block'>
-              <Image src={assets.seleoCover} alt='cover-image' className='object-cover h-full w-full'/>
-          </div>}
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.3 }} // Trigger animation once when 50% visible
+          variants={{
+            hidden: { opacity: 0, y: 50 }, // Start below and invisible
+            visible: {
+              opacity: 1,
+              y: 0,
+              transition: { duration: 1.3, ease: "easeOut" },
+            }, // Slide up and fade in
+          }}
+        >
+          <div className="flex flex-col gap-2 py-[50px] lg:w-[70%] lg:gap-6 lg:py-[142px]">
+            <div className="flex items-center gap-2">
+              <h4 className="text-30 leading-[1.5]">STORY</h4>
+              <div className="h-4 w-4 bg-primary lg:h-5 lg:w-5"></div>
+            </div>
 
-        </div>
-
-        {<div className='bg-black h-full lg:hidden'>
-              <Image src={assets.seleoCover} alt='cover-image' className='object-cover h-full w-full'/>
-          </div>}
-
+            <div>
+              <p className="fnt-lexend text-font19 text-gray1">
+                Seleo, a prominent Middle Eastern beverage brand, sought to
+                expand its digital footprint across the UAE, Kuwait, and Qatar.
+                To achieve this, they partnered with Global Surf to revitalize
+                their online presence and optimize marketing campaigns. The goal
+                was to create a compelling digital experience that resonated
+                with their target audience and drove customer engagement.
+              </p>
+            </div>
+          </div>
+        </motion.div>
       </div>
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.3 }} // Trigger animation once when 50% visible
+        variants={{
+          hidden: { opacity: 0, y: 50 }, // Start below and invisible
+          visible: {
+            opacity: 1,
+            y: 0,
+            transition: { duration: 1.3, ease: "easeOut" },
+          }, // Slide up and fade in
+        }}
+      >
+        <div className="flex w-full flex-col overflow-hidden  bg-bglight">
+          <div className="container relative">
+            <motion.div
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true, amount: 0.3 }} // Trigger animation once when 50% visible
+              variants={{
+                hidden: { opacity: 0, y: 50 }, // Start below and invisible
+                visible: {
+                  opacity: 1,
+                  y: 0,
+                  transition: { duration: 1.5, ease: "easeOut" },
+                }, // Slide up and fade in
+              }}
+            >
+              <div className={"flex h-full "}>
+                <div className="flex w-full  flex-col py-[50px]  lg:w-1/4 lg:py-[150px]">
+                  <div className=":lgpb-[40px] mb-[20px] flex flex-col border-b border-black pb-[20px] lg:mb-[36px]  lg:pb-[40px]">
+                    <h5 className="text-19 fnt-lexend pb-[5px] text-gray-400">
+                      Industry
+                    </h5>
+                    <h4 className="text-30">Food & Beverage</h4>
+                  </div>
 
+                  <div className="mb-[20px] flex flex-col border-b border-black pb-[20px] lg:mb-[36px] lg:pb-[40px]">
+                    <h5 className="text-19 fnt-lexend pb-[5px] text-gray-400">
+                      Country
+                    </h5>
+                    <h4 className="text-30">UAE, Kuwait, Qatar</h4>
+                  </div>
+
+                  <div className="flex flex-col border-black  ">
+                    <h5 className="text-19 fnt-lexend pb-[5px] text-gray-400">
+                      Channels Used
+                    </h5>
+                    <h4 className="text-30">Website - Google Ads - META Ads</h4>
+                  </div>
+                </div>
+              </div>
+            </motion.div>
+
+            {
+              <div className="absolute left-1/3 right-[-145px] top-0 hidden h-full bg-black lg:block">
+                <Image
+                  src={assets.seleoCover}
+                  alt="cover-image"
+                  className="h-full w-full object-cover"
+                />
+              </div>
+            }
+          </div>
+
+          {
+            <div className="h-full bg-black lg:hidden">
+              <Image
+                src={assets.seleoCover}
+                alt="cover-image"
+                className="h-full w-full object-cover"
+              />
+            </div>
+          }
+        </div>
       </motion.div>
     </>
-  )
-}
+  );
+};
 
-export default HeroSection
+export default HeroSection;
