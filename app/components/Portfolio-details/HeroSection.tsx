5c0c339faff523fd7b197caa7944e67e887dac34
jishnum007 <jishnu@globalsurf.ae>
2024-12-27 15:14:07 +0530

    portfolio details

--- app/components/Portfolio-details/HeroSection.tsx
+++ app/components/Portfolio-details/HeroSection.tsx
@@ -27,10 +27,9 @@ const HeroSection = ({
             className="absolute h-full w-full object-cover object-center"
             fill
           />
-        </div>
-        <div className="bg-bl-gradient absolute top-0 h-full w-full ">
+          <div className="bg-bl-gradient absolute top-0 h-full w-full ">
           <motion.div
-            className="container relative h-full w-full"
+            className="container relative h-full w-full invic"
             initial="hidden"
             whileInView="visible"
             viewport={{ once: true, amount: 0.3 }} // Trigger animation once when 50% visible
@@ -50,6 +49,8 @@ const HeroSection = ({
             />
           </motion.div>
         </div>
+        </div>
+
       </div>
 
       <div className="relative">
@@ -120,7 +121,7 @@ const HeroSection = ({
                 <h3 className="text-30 leading-lh1p33">STORY</h3>
                 <div className="h-4 w-4 bg-primary lg:h-5 lg:w-5 "></div>
               </div>
-              <div>
+              <div className='text-19 text-gray1 fnt-lexend'>
                 {/* <p className='text-19 text-gray1 fnt-lexend'>Innovo Group is a global leader in the construction industry,
                         specializing in delivering innovative solutions for a wide range of projects.
                         With a commitment to quality and sustainability, they have built a reputation
@@ -133,7 +134,7 @@ const HeroSection = ({
             </motion.div>
           </div>
         </div>
- 
+
     </div>
 
     <div className='bg-black'>
@@ -148,13 +149,13 @@ const HeroSection = ({
                                      }}>
                             <div className='lg:grid lg:grid-cols-3 lg:py-[120px] py-[20px] flex flex-col gap-5 lg:gap-5'>
                                 {data.portfolioHighlights.map((item)=>(
-                                    <div className='border-b lg:border-r lg:border-b-0 border-gray1 py-[20px] lg:py-[44px] group ' key={item.customId}>
+                                    <div className='border-b last:border-b-0 lg:border-b-0   lg:border-r last:border-r-0 border-gray1 py-[20px] lg:py-[44px] group ' key={item.customId}>
                                     <p className='title-65 text-primary duration-500 group-hover:-translate-y-1'>{item.number}</p>
                                     <h3 className='text-30 text-white lg:pt-[30px] pt-[10px] duration-500 group-hover:-translate-y-2'>{item.text}</h3>
                                     </div>
                                 ))}
-                                
- 
+
+
 
               {/* <div className='border-b lg:border-r border-gray1 lg:border-b-0 py-[20px] lg:py-[44px] group '>
                                     <div className='mx-auto lg:w-[85%]'>
