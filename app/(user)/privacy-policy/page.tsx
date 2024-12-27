6a459670a74dcb55880f4fdfd20f176f5d32daaa
jishnum007 <jishnu@globalsurf.ae>
2024-12-27 12:23:47 +0530

    policy

---
+++ app/(user)/privacy-policy/page.tsx
@@ -0,0 +1,135 @@
+import React from "react";
+import Link from "next/link";
+
+const page = () => {
+  return (
+    <>
+      <div className="container px-4">
+        <div className="py-[50px] lg:py-[100px]">
+          <p className=" text-font19">GS.Digital</p>
+          <h1 className="mb-5 text-font65"> Privacy Policy </h1>
+          <p className="mb-2 max-w-[44ch] text-font35">
+            <strong>
+              At GS.Digital, we are committed to protecting the privacy and
+              personal information of our users.{" "}
+            </strong>
+          </p>
+          <div className=" mb-5 grid grid-cols-1 gap-5">
+            <p className=" ">
+              This Privacy Policy informs you about how we use any personal data
+              you provide to us, including through our website globalsurf.ae
+              (the “Website”). The Website is owned and operated by Global Surf
+              Digital Media under the brand name GS.Digital, a company
+              incorporated and registered in Dubai, UAE, with its registered
+              office at 901, SIT Tower, Dubai Silicon Oasis, Dubai, UAE
+              (referred to as “we,” “our,” “us,” and “GS.Digital”).
+            </p>
+          </div>
+          <div className="mb-2">
+            <p className=" text-font35">Information Collection and Use </p>
+          </div>
+          <div className=" grid grid-cols-1 gap-2 mb-5">
+            <p className="  text-font19">
+              <strong>Personal Information  </strong>
+            </p>
+            <div  >
+              <p >We may collect personal information such as your name, email address, and contact details when you voluntarily provide it to us. This can occur when you subscribe to our newsletter, participate in surveys, or engage with our customer support. Rest assured, we only collect the necessary data to deliver our services and enhance your experience. </p>
+            </div>
+          </div>
+          <div className=" grid grid-cols-1 gap-2 mb-5">
+            <p className="  text-font19">
+              <strong>Non-Personal Information   </strong>
+            </p>
+            <div  >
+              <p >GS.Digital automatically collects certain non-personal information when you visit our website or use our digital media platforms. This includes your IP address, browser type, operating system, and general location data. This information helps us analyze trends, administer the site, and gather broad demographic information for aggregate use. </p>
+            </div>
+          </div>
+          <div className=" grid grid-cols-1 gap-2 mb-5">
+            <p className="  text-font19">
+              <strong>Cookies and Tracking Technologies     </strong>
+            </p>
+            <div  >
+              <p >We employ cookies and similar tracking technologies to enhance your browsing experience and gather valuable insights. These technologies allow us to remember your preferences, analyze site traffic, and personalize content. You can control the use of cookies through your browser settings, but please note that disabling them may limit certain functionalities on our website. </p>
+            </div>
+          </div>
+          <div className=" grid grid-cols-1 gap-5 mb-5">
+            <p className="  text-font19">
+              <strong>How We Use Your Information </strong>
+            </p>
+            <p>Your personal information is used for the following purposes: </p>
+            <div  >
+              <ul className="list-disc pl-[40px] list-outside">
+                <li>
+                Service Provision: To deliver the digital media services and content you have requested or subscribed to.
+                </li>
+                <li>
+                Communication: To send you important updates, promotional materials, or responses to your inquiries. You can opt out of receiving promotional content at any time.
+                </li>
+                <li>
+                Improvement: To enhance our website, services, and overall user experience based on the data we collect.
+                </li>
+                <li>
+                Legal Compliance: To comply with legal obligations and protect our rights and the rights of our users.
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className=" grid grid-cols-1 gap-5 mb-5">
+            <p className="  text-font19">
+              <strong>Data Sharing and Disclosure </strong>
+            </p>
+            <p>We do not sell, trade, or rent your personal information to third parties for their marketing purposes. However, we may share your data in the following circumstances:  </p>
+            <div  >
+              <ul className="list-disc pl-[40px] list-outside">
+                <li>
+                Service Providers: With trusted third-party service providers who assist us in operating our business and delivering our services. These partners are contractually bound to maintain the confidentiality of your information.
+                </li>
+                <li>
+                Legal Requirements: If required by law or to respond to valid legal processes, we may disclose your information to comply with legal obligations.
+                </li>
+                <li>
+                Business Transfers: In the event of a merger, acquisition, or sale of assets, your personal information may be transferred as part of the transaction.
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className=" grid grid-cols-1 gap-2 mb-5">
+            <p className="  text-font19">
+              <strong>Your Choices and Rights </strong>
+            </p>
+            <div  >
+              <p >You have the right to access, correct, or delete your personal information held by GS.Digital. You can also opt out of receiving marketing communications from us. To exercise these rights, please contact marketing@globalsurf.ae. </p>
+            </div>
+          </div>
+          <div className=" grid grid-cols-1 gap-2 mb-5">
+            <p className="  text-font19">
+              <strong>Data Security  </strong>
+            </p>
+            <div  >
+              <p >We implement reasonable security measures to protect your personal information from unauthorized access, disclosure, alteration, or destruction. However, please note that no method of transmission over the internet or electronic storage is 100% secure. </p>
+            </div>
+          </div>
+          <div className=" grid grid-cols-1 gap-2 mb-5">
+            <p className="  text-font19">
+              <strong>Changes to This Privacy Policy </strong>
+            </p>
+            <div  >
+              <p >We reserve the right to update or modify this Privacy Policy at any time. Any changes will be effective immediately upon posting the revised policy on our website. We encourage you to review this page periodically for any updates. </p>
+            </div>
+          </div>
+          <div className=" grid grid-cols-1 gap-2 mb-5">
+            <p className="  text-font19">
+              <strong>Contact Us </strong>
+            </p>
+            <div  >
+              <p className="  ">If you have any questions, concerns, or feedback regarding this Privacy Policy or our data practices, please reach out to our dedicated team at marketing@globalsurf.ae </p>
+              <p className="  ">Thank you for choosing GS.Digital, and we look forward to providing you with an exceptional digital media experience while respecting your privacy. </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default page;
