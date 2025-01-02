08d2b2d287c3daaaa6dc2a30f3ad686edb2f2357
Nived <nivuyt789@gmail.com>
2025-01-02 18:56:25 +0530

    fix:extra spaces

--- app/api/case-study/route.ts
+++ app/api/case-study/route.ts
@@ -293,7 +293,6 @@ export async function POST(req: NextRequest) {
                         tag,
                         categories:addedCategoriesRaw,
                         companyName
-
                     },
                 ])
                 .select('id')
