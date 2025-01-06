f122994ba815ae43b98ebcb7cf6165e6b372a60f
yasirtlr <yasirtlr@gmail.com>
2025-01-06 12:25:18 +0400

    url change

--- app/components/SuccessStories/data.ts
+++ app/components/SuccessStories/data.ts
@@ -5,18 +5,18 @@ export const data = [
         image:assets.success1,
         companyLogo:assets.gulfcryo,
         text:"Gulf Cryo",
-        url: "/portfolio-details/gulf-cryo"
+        url: "/portfolio/gulf-cryo"
     },
     {
         image:assets.success2,
         companyLogo:assets.ascs,
         text:"ASCS",
-        url: "/portfolio-details/american-school-of-creative-science"
+        url: "/portfolio/american-school-of-creative-science"
     },
     {
         image:assets.success3,
         companyLogo:assets.bec,
         text:"BEC",
-        url: "/portfolio-details/bec-arabia"
+        url: "/portfolio/bec-arabia"
     }
 ]
