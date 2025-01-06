6f0814131a3ac1b10ff391b502d454c323298b15
jishnum007 <jishnu@globalsurf.ae>
2025-01-06 12:55:20 +0530

    changes

--- app/data/stories.ts
+++ app/data/stories.ts
@@ -9,7 +9,7 @@ export const stories = [
     title2: "The Story Behind Garden Concept’s 77.54% Traffic Growth",
     description2:
       "Explore The Garden Concept’s digital marketing strategy for boosting overall web traffic and store visit. ",
-      url: "case-study/regaining-overall-traffic-for-garden-concept-through-strategic-digital-marketing",
+      url: "case-study/the-garden-concept",
   },
   {
     title1: "Seleo",
@@ -19,7 +19,7 @@ export const stories = [
     title2: "The Secrets Behind Seleo’s 10x Revenue Growth ",
     description2:
       "Discover how Seleo achieved remarkable growth through innovative strategies that enhanced online engagement and business success. ",
-      url: "case-study/inside-seleos-10x-revenue-growth-transformation",
+      url: "case-study/seleo",
   },
   {
     title1: "Assent Steel ",
@@ -29,6 +29,6 @@ export const stories = [
     title2: "How Assent Steel Doubled Its Website Traffic ",
     description2:
       "Explore Inside Assent Steel’s 2x Traffic Success and uncover how smart strategies and data-driven tactics doubled their website traffic. ",
-      url: "case-study/the-strategy-behind-assent-steels-2x-traffic-growth",
+      url: "case-study/assent-steel",
   },
 ];
