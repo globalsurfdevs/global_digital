0bfd6ddca38a898593570aa103caeb83dd8750f1
jishnum007 <jishnu@globalsurf.ae>
2025-01-06 18:03:38 +0530

    gtm

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "../../app/globals.css";
+import Head from "next/head";
 import Header from "@/app/components/HomePage/Header";
 import { Space_Grotesk } from "next/font/google";
 import Footer from "@/app/components/HomePage/Footer";
@@ -20,7 +21,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>    <script dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','GTM-TVWCC3XC');`, }} /></Head>
       <body>
+         <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TVWCC3XC"height="0"width="0"style={{ display: "none", visibility: "hidden" }}           /></noscript>
         <Header />
         {children}
         <Footer />
