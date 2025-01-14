8c3957b3a15247d6831c60ff464351746905f24a
jishnum007 <jishnu@globalsurf.ae>
2025-01-14 11:58:56 +0530

    career

--- app/data/career.ts
+++ app/data/career.ts
@@ -20,28 +20,22 @@ export const career = [
     post: "HR Recruiter ",
     team: "HR Team",
     url: "hr-recruiter",
-  }
-  // {
-  //   id:"1",
-  //   post: "Sales Specialist - Web and Digital Services",
-  //   team: "Marketing Team",
-  //   url: "sales-specialist-web-digital",
-  //   slug: "sales-specialist-web-digital",
+  },
+  {
+    id:"4",
+    post: "Sales Specialist - Web and Digital Services",
+    team: "Marketing Team",
+    url: "sales-specialist-web-digital",
+    slug: "sales-specialist-web-digital",
 
 
-  // },
-  // {
-  //   id:"2",
-  //   post: "Content Writer",
-  //   team: "Content Writing Team",
-  //   url: "content-writer",
-  //   slug: "content-writer",
-  // },
-  // {
-  //   id:"3",
-  //   post: "Performance Marketing Specialist",
-  //   team: "Digital Marketing Team",
-  //   url: "performance-marketing-specialist",
-  //   slug: "performance-marketing-specialist",
-  // },
+  },
+
+  {
+    id:"5",
+    post: "Performance Marketing Specialist",
+    team: "Digital Marketing Team",
+    url: "performance-marketing-specialist",
+    slug: "performance-marketing-specialist",
+  },
 ];
