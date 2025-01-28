639d4e47c60293e9dae79a448ce76bea7555a98e
sreemayispraseed <sreemayi99@gmail.com>
2025-01-28 14:21:55 +0530

    webdesign & dev , branding and marketing intelligence pages changes updated - h1 content updated

--- app/data/services/web-design-development/herosection.ts
+++ app/data/services/web-design-development/herosection.ts
@@ -4,9 +4,12 @@ export const BannerSection = [
   {
     id: 1,
     image: assets.web_bnr,
-    title: "Web Design & Development",
+    title: "Transform Your Business with Expert Web Solutions in Dubai",
     sub: [
-      { stitle: "OUR APPROACH", desc: 'Create a responsive website that establishes a powerful digital presence, delivering a seamless user experience. Engage your audience and highlight your unique offerings, ensuring visitors connect with your brand and easily navigate your site, all while enhancing overall customer satisfaction. ' },
+      {
+        stitle: "OUR APPROACH",
+        desc: "Create a responsive website that establishes a powerful digital presence, delivering a seamless user experience. Engage your audience and highlight your unique offerings, ensuring visitors connect with your brand and easily navigate your site, all while enhancing overall customer satisfaction. ",
+      },
     ],
-  }
+  },
 ];
