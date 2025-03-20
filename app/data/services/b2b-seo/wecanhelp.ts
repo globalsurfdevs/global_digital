951c829ffbf4f8faa10c23d4fef39539de95b03d
sreemayispraseed <sreemayi99@gmail.com>
2025-03-20 18:08:27 +0530

    b2b-seo-agency-dubai - a tag color changed

--- app/data/services/b2b-seo/wecanhelp.ts
+++ app/data/services/b2b-seo/wecanhelp.ts
@@ -1,4 +1,3 @@
-
 import { assets } from "@/public/assets/assets";
 
 export const Wecanhelp = {
@@ -7,32 +6,32 @@ export const Wecanhelp = {
     {
       id: 1,
       icon: assets.frb1,
-      title:"Increase Brand Visibility",
+      title: "Increase Brand Visibility",
       dec: "Enhance your brand’s online presence and reach key decision-makers.",
     },
     {
       id: 2,
       icon: assets.frb2,
-      title:"Drive Qualified Leads",
+      title: "Drive Qualified Leads",
       dec: "Attract high-quality leads that convert into loyal customers. ",
     },
     {
       id: 3,
       icon: assets.frb3,
-      title:"Boost Organic Traffic",
+      title: "Boost Organic Traffic",
       dec: "Improve your website’s ranking and drive relevant traffic. ",
     },
     {
       id: 4,
       icon: assets.frb4,
-      title:"Strengthen Credibility",
+      title: "Strengthen Credibility",
       dec: "Establish your brand as a trusted authority in your industry.",
     },
     {
-      id:5,
+      id: 5,
       icon: assets.frb5,
-      title:"Maximize ROI",
-      dec: "Achieve long-term growth with <a href='https://www.globalsurf.ae/seo-agency-dubai' class='text-primary opacity-[.8] hover:opacity-[1]'>cost-effective SEO strategies.</a>",
+      title: "Maximize ROI",
+      dec: "Achieve long-term growth with <a href='https://www.globalsurf.ae/seo-agency-dubai' class=''>cost-effective SEO strategies.</a>",
     },
   ],
 };
