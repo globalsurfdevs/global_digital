2d6a66e949730df052c4a381bbb9d01d38c72697
sreemayispraseed <sreemayi99@gmail.com>
2025-03-20 17:59:33 +0530

    seo and creative agency pages urls added under expertise section

--- app/data/services/branding-creative/area-of-expertise.ts
+++ app/data/services/branding-creative/area-of-expertise.ts
@@ -1,4 +1,3 @@
-
 import { assets } from "@/public/assets/assets";
 
 export const AreaExpertise = {
@@ -9,33 +8,42 @@ export const AreaExpertise = {
       icon: assets.sm_strategy,
       title: "Branding   ",
       desc: "Building a cohesive brand that tells your story, captures attention, and creates a lasting impression. ",
+      url: "branding-agency-in-dubai",
     },
     {
       id: 2,
       icon: assets.sm_management,
       title: "Logo Design",
       desc: "Distinctive logos that encapsulate your brand’s essence and make your mark unforgettable. ",
+      url: "",
     },
     {
       id: 3,
       icon: assets.sm_marketing,
       title: "Video Production  ",
       desc: "Compelling videos that bring your ideas to life, connect with your audience, and amplify your message. ",
-    },{
+      url: "",
+    },
+    {
       id: 3,
       icon: assets.sm_marketing,
       title: "Photography   ",
       desc: "Stunning imagery that captures your brand’s personality and tells a story worth sharing. ",
-    },{
+      url: "",
+    },
+    {
       id: 3,
       icon: assets.sm_marketing,
       title: "Copywriting   ",
       desc: "Persuasive, engaging words that not only speak but sell—bringing your brand’s voice to life. ",
-    },{
+      url: "",
+    },
+    {
       id: 3,
       icon: assets.sm_marketing,
       title: "Graphic Design   ",
       desc: "Striking visuals that enhance your brand’s appeal and leave a lasting impression. ",
+      url: "",
     },
   ],
 };
