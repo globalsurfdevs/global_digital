62d9e532cc63c573dfada8ee57865c18863c6836
sreemayispraseed <sreemayi99@gmail.com>
2025-03-24 17:57:34 +0530

    industry-construction page created

---
+++ app/data/services/ind-constructions/industries-we-serve.ts
@@ -0,0 +1,36 @@
+import { assets } from "@/public/assets/assets";
+import { url } from "inspector";
+
+export const IndustriesWeServe = {
+  title: "Industries We Serve",
+  data: [
+    {
+      id: 1,
+      icon: assets.engineering,
+      title: "Engineering ",
+      desc: "",
+      url: "",
+    },
+    {
+      id: 2,
+      icon: assets.e_commerce,
+      title: "E-commerce",
+      desc: "",
+      url: "",
+    },
+    {
+      id: 3,
+      icon: assets.education,
+      title: "Education",
+      desc: "",
+      url: "",
+    },
+    {
+      id: 4,
+      icon: assets.healthcare,
+      title: "Health care",
+      desc: "",
+      url: "",
+    },
+  ],
+};
