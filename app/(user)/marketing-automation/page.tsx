eb30d1a37bbd6549adf31cee48f71352355de4bb
jishnum007 <jishnu@globalsurf.ae>
2025-04-28 13:43:45 +0530

    marketing automation

---
+++ app/(user)/marketing-automation/page.tsx
@@ -0,0 +1,96 @@
+import React from "react";
+import HeroSection from "../../components/PermormanceMarketing/HeroSection";
+import Services from "../../components/PermormanceMarketing/Services";
+import FAQ from "../../components/PermormanceMarketing/FAQ";
+import GetInTouch from "../../components/PermormanceMarketing/GetInTouch";
+import LogoSwiper from "../../components/MarketingAutomation/LogoSwiper";
+
+import {
+  BannerSection,
+  Wecanhelp,
+  OurServices,
+  Frameworkdata,
+  Platformsecomdata,
+  Cta,
+  Faq,
+  relatedservices
+} from "../../components/MarketingAutomation/data";
+
+import Platformserver from "@/app/components/e-commerce-wdd/Platformserver";
+import Framework from "@/app/components/PermormanceMarketing/Framework";
+import Platforms from "@/app/components/PermormanceMarketing/Platforms";
+import RelatedServices from "@/app/components/eCommerceSeoDubai/RelatedServices";
+
+interface Canonicals {
+  canonical: string;
+}
+type Metadata = {
+  title: string;
+  description: string;
+  alternates: Canonicals;
+  robots: string;
+};
+
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: "Digital Marketing Services",
+    description:
+      "Partner with a top web app development company in Dubai specializing in custom web applications. We build secure web apps tailored to your business. ",
+    alternates: {
+      canonical: "https://www.globalsurf.ae/",
+    },
+    robots: "noindex, nofollow",
+  };
+}
+
+const page = () => {
+  return (
+    <div>
+      <HeroSection
+        Bannerdata={BannerSection}
+        hideslider={true}
+        maxchwidth={34}
+      />
+
+<Platforms title={Wecanhelp.title} data={Wecanhelp.data} icontitle={true} hiddentitle={true} leftzero={true} colcount={3} />
+
+        <Framework
+          title={Frameworkdata.title}
+          data={Frameworkdata.data}
+          bgcolor="white"
+          colcount={4}
+        />
+
+      <Services
+        title={OurServices.title}
+        data={OurServices.data}
+        colcount={5}
+        bgcolor="bg-black"
+        bgtt1="text-white"
+        bgtt3="text-white"
+        hrcontent={true}
+      />
+      <div className="pb-[50px] pt-[50px] lg:pb-[130px] lg:pt-[130px]">
+        <LogoSwiper
+        />
+      </div>
+
+        <Platformserver
+        title={Platformsecomdata.title}
+        desc={Platformsecomdata.desc}
+          data={Platformsecomdata.data}
+        />
+
+      <GetInTouch
+        data={Cta}
+        redlast={true}
+        ctabbutton={"Let’s Connect!"}
+        link={"/lets-talk"}
+      />
+      <FAQ data={Faq} />
+      <RelatedServices title={relatedservices.title}   data={relatedservices.data} colcount={3} />
+    </div>
+  );
+};
+
+export default page;
---
+++ app/components/MarketingAutomation/data.ts
@@ -0,0 +1,368 @@
+import { assets } from "@/public/assets/assets";
+import { url } from "inspector";
+export const BannerSection = [
+  {
+    id: 1,
+    image: assets.massbanner,
+    title:
+      "Automate Tasks, Target Audiences & Personalize Outreach With Expert Marketing Automation Specialists in Dubai",
+    sub: [
+      {
+        stitle: "OUR APPROACH",
+        desc: "As a leading marketing automation agency in Dubai, we implement AI-driven solutions to streamline customer interactions and enhance campaign efficiency. Making use of data analytics, machine learning, and predictive modeling, we optimize workflows, automate lead nurturing, and refine engagement strategies, so you can focus on scaling your business with precision and impact.",
+      },
+    ],
+  },
+];
+export const Wecanhelp = {
+  title: "We Can Help You ",
+  data: [
+    {
+      id: 1,
+      icon: assets.hels1,
+      dec: "Streamline engagement processes",
+    },
+    {
+      id: 2,
+      icon: assets.hels2,
+      dec: "Boost conversion rates ",
+    },
+    {
+      id: 3,
+      icon: assets.hels3,
+      dec: "Personalized experiences at scale ",
+    },
+    {
+      id: 4,
+      icon: assets.hels4,
+      dec: "Optimize spending efficiency",
+    },
+    {
+      id: 5,
+      icon: assets.hels5,
+      dec: "Precise performance insights",
+    },
+  ],
+};
+
+export const Frameworkdata = {
+  title: "Our Marketing Automation Solutions",
+  data: [
+    {
+      id: 1,
+      title: "Email Marketing Automation",
+      dec: "Create personalized, behavior-driven email workflows that engage customers, boost retention, and drive higher conversions.",
+    },
+    {
+      id: 2,
+      title: "Lead Nurturing & Scoring ",
+      dec: "Automated lead segmentation, tracking, and scoring to ensure high-intent leads seamlessly move through your sales funnel. ",
+    },
+    {
+      id: 3,
+      title: "CRM Integration & Data Synchronization",
+      dec: "Effortlessly connect your CRM with marketing automation tools to sync customer data, enhance personalization, and improve campaign efficiency.",
+    },
+    {
+      id: 4,
+      title: "Multi-Channel Campaign Automation",
+      dec: "Automate personalized interactions across email, social media, SMS, push notifications, and web notifications to deliver a cohesive and impactful brand experience.",
+    },
+    {
+      id: 5,
+      title: "AI-Powered Chatbots & Conversational Marketing",
+      dec: "Engage customers instantly with AI-driven chatbots and virtual assistants that provide real-time support, answer queries, and drive conversions.",
+    },
+    {
+      id: 6,
+      title: "Customer Journey Optimization & Personalization  ",
+      dec: "Leverage automation to map, analyze, and refine customer touchpoints, ensuring a frictionless and engaging user experience that boosts loyalty and conversions.",
+    },
+    {
+      id: 7,
+      title: "Analytics & Performance Tracking",
+      dec: "Gain deep insights into customer interactions, campaign performance, and engagement metrics with advanced analytics, enabling data-driven marketing decisions.",
+    },
+  ],
+};
+export const OurServices = {
+  title: "Our Marketing Automation Process",
+  data: [
+    {
+      id: 1,
+      title: "Marketing Automation Roadmap ",
+      desc: "A step-by-step approach to integrating automation for smoother customer interactions. ",
+    },
+    {
+      id: 2,
+      title: "Solution Architecture Design",
+      desc: "Building a solid framework to ensure your marketing automation runs seamlessly. ",
+    },
+    {
+      id: 3,
+      title: "Implementing Marketing Automation",
+      desc: "Setting up the right tools and workflows to make your marketing more efficient. ",
+    },
+    {
+      id: 4,
+      title: "Campaign Management",
+      desc: "Planning, executing, and optimizing campaigns to drive better engagement. ",
+    },
+    {
+      id: 5,
+      title: "Data Analysis & Visualization ",
+      desc: "Turning data into clear insights to help you make smarter marketing decisions. ",
+    },
+  ],
+};
+
+export const Matslogo = [
+  {
+    id: 1,
+    src: assets.mats1,
+    alt: "Logo",
+  },
+  {
+    id: 2,
+    src: assets.mats2,
+    alt: "Logo",
+  },
+  {
+    id: 3,
+    src: assets.mats3,
+    alt: "Logo",
+  },
+  {
+    id: 4,
+    src: assets.mats4,
+    alt: "Logo",
+  },
+  {
+    id: 5,
+    src: assets.mats5,
+    alt: "Logo",
+  },
+  {
+    id: 6,
+    src: assets.mats6,
+    alt: "Logo",
+  },
+  {
+    id: 7,
+    src: assets.mats7,
+    alt: "Logo",
+  },
+  {
+    id: 8,
+    src: assets.mats8,
+    alt: "Logo",
+  },
+  {
+    id: 9,
+    src: assets.mats9,
+    alt: "Logo",
+  },
+  {
+    id: 10,
+    src: assets.mats10,
+    alt: "Logo",
+  },
+  {
+    id: 11,
+    src: assets.mats11,
+    alt: "Logo",
+  },
+  {
+    id: 12,
+    src: assets.mats12,
+    alt: "Logo",
+  },
+  {
+    id: 13,
+    src: assets.mats13,
+    alt: "Logo",
+  },
+  {
+    id: 14,
+    src: assets.mats14,
+    alt: "Logo",
+  },
+  {
+    id: 15,
+    src: assets.mats15,
+    alt: "Logo",
+  },
+  {
+    id: 16,
+    src: assets.mats16,
+    alt: "Logo",
+  },
+  {
+    id: 17,
+    src: assets.mats17,
+    alt: "Logo",
+  },
+  {
+    id: 18,
+    src: assets.mats18,
+    alt: "Logo",
+  },
+  {
+    id: 19,
+    src: assets.mats19,
+    alt: "Logo",
+  },
+  {
+    id: 20,
+    src: assets.mats20,
+    alt: "Logo",
+  },
+  {
+    id: 21,
+    src: assets.mats21,
+    alt: "Logo",
+  },
+  {
+    id: 22,
+    src: assets.mats22,
+    alt: "Logo",
+  },
+  {
+    id: 23,
+    src: assets.mats23,
+    alt: "Logo",
+  },
+  {
+    id: 24,
+    src: assets.mats24,
+    alt: "Logo",
+  },
+  {
+    id: 25,
+    src: assets.mats25,
+    alt: "Logo",
+  },
+];
+export const Platformsecomdata = {
+  title: "Why Choose GS Digital as Your Marketing Automation Partner? ",
+  desc: "With a team of marketing strategists and automation experts, we create customized solutions that help businesses grow smarter, faster, and more efficiently.",
+  data: [
+    {
+      id: 1,
+      icon: assets.google,
+      title: "100",
+      rsubtitle: "Successful<br>Campaigns ",
+      dec: "100+ Successful Campaigns, helping businesses optimize marketing performance with automation.",
+    },
+    {
+      id: 2,
+      icon: assets.linkedin,
+      title: "12",
+      rsubtitle: "Dedicated<br>Professionals  ",
+      dec: "12+ Dedicated Professionals focused on streamlining marketing operations and driving engagement.",
+    },
+    {
+      id: 3,
+      icon: assets.twitterx,
+      title: "11",
+      rsubtitle: "Years of<br>Excellence ",
+      dec: "11+ Years of Excellence Over a decade of expertise in martech solutions that combine innovation, performance, and long-term success.",
+    },
+  ],
+};
+
+export const Cta = [
+  {
+    textred: "drive smarter growth. ",
+    text: "Automate campaigns, analyze performance, and ",
+  },
+];
+export const Faq = [
+  {
+    title: "What is marketing automation, and how can it benefit my business? ",
+    description:
+      "Marketing automation uses technology to streamline and optimize marketing tasks, improving efficiency, lead generation, and customer engagement. ",
+  },
+  {
+    title: "What marketing automation tools do you use? ",
+    description:
+      "We work with HubSpot, Marketo, Pardot, Mailchimp, ActiveCampaign, and more, depending on your business needs. ",
+  },
+  {
+    title: "Can marketing automation help with lead generation? ",
+    description:
+      "Absolutely! We use automation to segment leads, personalize communication, and nurture prospects effectively. ",
+  },
+  {
+    title: "How long does it take to implement a marketing automation system? ",
+    description:
+      "Implementation timelines vary based on complexity but typically range from 4-8 weeks. ",
+  },
+  {
+    title: " Is marketing automation suitable for small businesses?",
+    description:
+      "Yes! Automation helps businesses of all sizes save time, reduce costs, and improve customer engagement. ",
+  },
+  {
+    title: "How do you ensure data security in marketing automation? ",
+    description:
+      "We implement encryption, secure integrations, and compliance measures to safeguard customer data. ",
+  },
+  {
+    title: "Will my team need training to use marketing automation?",
+    description:
+      "Yes, and we provide comprehensive training and ongoing support to ensure smooth adoption. ",
+  },
+  {
+    title: "Can you integrate automation with my existing CRM? ",
+    description:
+      "Yes! We ensure seamless integration with leading CRM platforms to enhance marketing efficiency",
+  },
+  {
+    title: "What industries benefit from marketing automation? ",
+    description:
+      "Industries including e-commerce, real estate, healthcare, education, and finance see significant advantages from automation. ",
+  },
+];
+export const relatedservices = {
+  title: "Related Services",
+  data: [
+    {
+      id: 1,
+      title: "SEO & Performance Marketing ",
+      dec: "We use data-driven strategies to boost your online presence and attract the right audience. ",
+      icon:assets.rsd1
+    } ,
+    {
+      id: 2,
+      title: "Email Marketing ",
+      dec: "Our personalized campaigns help nurture leads and keep your customers engaged. ",
+      icon:assets.rsd2
+    } ,
+    {
+      id: 3,
+      title: "Social Media Automation ",
+      dec: "Save time while effortlessly managing and optimizing your social media presence. ",
+      icon:assets.rsd3
+    } ,
+    {
+      id: 4,
+      title: "Strategy Consulting ",
+      dec: "We work with you to refine your digital strategy and drive long-term success. ",
+      icon:assets.rsd4
+    } ,
+    {
+      id: 5,
+      title: "Data Analytics ",
+      dec: "We turn complex data into clear insights to improve your marketing decisions. ",
+      icon:assets.rsd5
+    } ,
+    {
+      id: 6,
+      title: "Conversion Rate Optimization (CRO) ",
+      dec: " We fine-tune your website to turn visitors into loyal customers.",
+      icon:assets.rsd6
+    }
+  ],
+};
+
