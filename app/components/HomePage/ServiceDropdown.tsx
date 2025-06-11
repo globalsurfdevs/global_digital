96de5ffa1a90480d4d8e7f47d0839df76f18f8d8
Sreemayi-s-praseed <sreemayi@globalsurf.ae>
2025-06-11 12:22:43 +0530

    animation added

--- app/components/HomePage/ServiceDropdown.tsx
+++ app/components/HomePage/ServiceDropdown.tsx
@@ -1,8 +1,10 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { assets } from "@/public/assets/assets";
 import menuright from "@/public/assets/menurightarrow.svg";
 import Image from "next/image";
+import { motion } from "framer-motion";
+
 
 type ServiceItem = { text: string; url?: string };
 type ServiceCategory = {
@@ -13,6 +15,7 @@ type ServiceCategory = {
 type ServiceData = {
   [category: string]: ServiceCategory;
 };
+
 
 const serviceData: ServiceData = {
   "Creative Services": {
@@ -136,11 +139,24 @@ const ServicesMegaMenu = () => {
         <div className="w-full mx-auto px-8 xxl:py-[80px] py-[30px] flex justify-between items-center xxl:pl-[150px] lg:pl-[50px]">
           {/* Header Section */}
           <div className="w-1/3">
-            <h2
-              className="xxl:text-[48px] lg:text-[35px] xxl:leading-[60px] lg:leading-[50px] mb-4 transition-all duration-300"
+          
+           <motion.h2
+           initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.3 }} // Trigger animation once when 50% visible
+          variants={{
+            hidden: { opacity: 0, y: 50 }, // Start below and invisible
+            visible: {
+              opacity: 1,
+              y: 0,
+              transition: { duration: 1, ease: "easeOut" },
+            }, // Slide up and fade in
+          }}
+              className="xxl:text-[48px] lg:text-[35px] xxl:leading-[60px] lg:leading-[50px] mb-4 "
               dangerouslySetInnerHTML={{
                 __html: getActiveText(),
               }}
+              
             />
           </div>
 
@@ -151,7 +167,18 @@ const ServicesMegaMenu = () => {
               return (
                 <div key={category} className="group w-full">
                     <div className="flex items-center xxl:mb-[30px] mb-[20px]">
-                    <h4
+                    <motion.h4
+                    initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.3 }} // Trigger animation once when 50% visible
+          variants={{
+            hidden: { opacity: 0, y: 50 }, // Start below and invisible
+            visible: {
+              opacity: 1,
+              y: 0,
+              transition: { duration: 1, ease: "easeOut" },
+            }, // Slide up and fade in
+          }}
                       onMouseEnter={() => handleHover(category)}
                       className={`font-[400] uppercase xxl:text-font19 text-[15px] pr-[16px] ${
                       activeItem[0] === category
@@ -164,6 +191,17 @@ const ServicesMegaMenu = () => {
                         : category,
                       }}
                     />
+                    <motion.div  initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.3 }} // Trigger animation once when 50% visible
+          variants={{
+            hidden: { opacity: 0, y: 50 }, // Start below and invisible
+            visible: {
+              opacity: 1,
+              y: 0,
+              transition: { duration: 1, ease: "easeOut" },
+            }, // Slide up and fade in
+          }}>
                     <Image
                       src={menuright}
                       alt="arrow"
@@ -174,15 +212,27 @@ const ServicesMegaMenu = () => {
                         ? "invert(36%) sepia(92%) saturate(7492%) hue-rotate(349deg) brightness(97%) contrast(97%)"
                         : "",
                       }}
-                    />
+                    /></motion.div>
                     </div>
-                  <ul className="space-y-2">
+                  <motion.ul className="space-y-2"  initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.3 }} // Trigger animation once when 50% visible
+          variants={{
+            hidden: { opacity: 0, y: 50 }, // Start below and invisible
+            visible: {
+              opacity: 1,
+              y: 0,
+              transition: { duration: 1, ease: "easeOut" },
+            }, // Slide up and fade in
+          }}>
+                    
                     {Object.entries(services).map(([title, data]) => {
                       const item = data as ServiceItem;
                       const isActive =
                         activeItem[0] === category && activeItem[1] === title;
                       return (
                         <li
+                       
                           key={title}
                           onMouseEnter={() => handleHover(category, title)}
                           className={`cursor-pointer xxl:text-font19 text-[14px] transition-opacity duration-200 ${
@@ -195,7 +245,7 @@ const ServicesMegaMenu = () => {
                         </li>
                       );
                     })}
-                  </ul>
+                  </motion.ul>
                 </div>
               );
             })}
