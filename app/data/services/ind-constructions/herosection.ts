79e8bc1d31f525e00365b836ff1cddd253c259e2
Sreemayi-s-praseed <sreemayi@globalsurf.ae>
2025-07-14 18:46:47 +0530

    breadcrbs updated

--- app/data/services/ind-constructions/herosection.ts
+++ app/data/services/ind-constructions/herosection.ts
@@ -4,6 +4,11 @@ export const BannerSection = [
   {
     id: 1,
     image: assets.ind_construction,
+    navigation: [
+      { label: "Home", url: "/" },
+      { label: "Industry",url: "/industry" },
+      { label: "Construction" },
+    ],
     heroAlt: "Digital Marketing for the Construction Companies",
     title1: "Outrank, Outperform & Outgrow",
     title: "Put Your Construction Brand  on the Digital Map.",
