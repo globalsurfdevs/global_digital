36a3b1d0fcda5c569a825db058c3d80fdaab9869
Sreemayi-s-praseed <sreemayi@globalsurf.ae>
2026-02-19 14:53:07 +0530

    sitemap html page created. blogs meta tags bug fixed

--- app/(user)/blogs/[heading]/page.tsx
+++ app/(user)/blogs/[heading]/page.tsx
@@ -1,12 +1,81 @@
-import React from 'react'
-import BlogDetails from '@/app/components/Blog-details'
+import BlogDetails from "@/app/components/Blog-details";
+import { BlogData } from "@/app/data/BlogData";
+import { Metadata } from "next";
 
-const page = () => {
-  return (
-    <>
-        <BlogDetails/>
-    </>
-  )
+type Props = {
+  params: { heading: string };
+};
+
+// ✅ META TAGS
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const blog = BlogData.find((b) => b.slug === params.heading);
+  const canonicalUrl = `https://www.globalsurf.ae/blogs/${params.heading}`;
+
+  if (!blog) {
+    return {
+      title: "Blog Not Found",
+    };
+  }
+
+  return {
+    title: blog.meta_title,
+    description: blog.description,
+    alternates: {
+      canonical: canonicalUrl,
+    },
+    openGraph: {
+      title: blog.meta_title,
+      description: blog.description,
+      images: [
+        `https://www.globalsurf.ae${blog.feature_thumb}`, // ✅ must be absolute
+      ],
+    },
+  };
 }
 
-export default page
+// ✅ PAGE RENDER
+export default function Page({ params }: Props) {
+  const blog = BlogData.find((b) => b.slug === params.heading);
+
+  if (!blog) return null;
+
+  // 🔥 Dynamic JSON-LD Schema
+  const schema = {
+    "@context": "https://schema.org",
+    "@type": "Article",
+    mainEntityOfPage: {
+      "@type": "WebPage",
+      "@id": `https://www.globalsurf.ae/blogs/${blog.slug}`,
+    },
+    headline: blog.list_heading,
+    image: `https://www.globalsurf.ae${blog.feature_thumb}`,
+    author: {
+      "@type": "Organization",
+      name: "Global Surf Digital",
+    },
+    publisher: {
+      "@type": "Organization",
+      name: "Global Surf Digital",
+      logo: {
+        "@type": "ImageObject",
+        url: "https://www.globalsurf.ae/gs-digital-logo.svg",
+      },
+    },
+    datePublished: blog.post_date || "",
+    description: blog.description || "",
+  };
+
+  return (
+    <>
+      {/* ✅ JSON-LD Schema */}
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify(schema),
+        }}
+      />
+
+      <BlogDetails />
+    </>
+  );
+}
---
+++ app/(user)/sitemap/page.tsx
@@ -0,0 +1,9 @@
+import SitemapList  from "@/app/components/Sitemap/SitemapList";
+
+export default function Page() {
+  return (
+    <>
+      <SitemapList />
+    </>
+  );
+}
