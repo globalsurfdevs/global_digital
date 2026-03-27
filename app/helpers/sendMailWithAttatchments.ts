77f04a2c8a282970ea4368baa7e9eded98d2d6b0
Nived <nived@globalsurf.ae>
2026-03-27 13:10:33 +0530

    fix:email sending issue forms

--- app/helpers/sendMailWithAttatchments.ts
+++ app/helpers/sendMailWithAttatchments.ts
@@ -1,6 +1,7 @@
 import { resend } from "./mailer";
 import { CareerTemplate } from "../../templates/careerTemplate";
 import type { ReactElement } from "react";
+import { ContactTemplate } from "@/templates/contactTemplate";
 
 interface Attachment {
   filename: string;
@@ -8,28 +9,43 @@ interface Attachment {
 }
 
 export async function sendMailWithAttachments({
+  type,
   to,
   subject,
   fields,
   attachments,
 }: {
+  type: string;
   to: string | string[];
   subject: string;
   fields: any;
-  attachments: Attachment[];
+  attachments?: Attachment[];
 }) {
-  const { data, error } = await resend.emails.send({
-    from: "Enquiry <enquiry@globalsurf.ae>",
-    to,
-    subject,
-    react: CareerTemplate(fields) as ReactElement,
-    attachments,
-  });
 
-  if (error) {
-    console.error("Resend error:", error);
-    throw new Error("Failed to send email");
+  if (type == "career") {
+    const { error } = await resend.emails.send({
+      from: "Enquiry <enquiry@globalsurf.ae>",
+      to,
+      subject,
+      react: CareerTemplate(fields) as ReactElement,
+      attachments,
+    });
+
+    if (error) {
+      console.error("Resend error:", error);
+      throw new Error("Failed to send email");
+    }
+  } else {
+    const { error } = await resend.emails.send({
+      from: "Enquiry <enquiry@globalsurf.ae>",
+      to,
+      subject,
+      react: ContactTemplate(fields) as ReactElement,
+    });
+
+    if (error) {
+      console.error("Resend error:", error);
+      throw new Error("Failed to send email");
+    }
   }
-
-  return data;
 }
