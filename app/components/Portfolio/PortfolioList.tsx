ca46de1be43e23dce74c4aced2483092b0946216
Hisham <hisham@globalsurf.ae>
2026-06-08 17:29:06 +0530

    fix: blogs, industries, portfolio (ssr related issues)

--- app/components/Portfolio/PortfolioList.tsx
+++ app/components/Portfolio/PortfolioList.tsx
@@ -6,10 +6,14 @@ import { useEffect, useState } from "react";
 import { Portfolio } from "@/app/types/Portfolio";
 import { filterTags } from "@/app/data/filterTags";
 import Link from "next/link";
-import { formatLinkForPortfolio, formatLinkForCaseStudy } from "@/app/helpers/formatLink";
+import {
+  formatLinkForPortfolio,
+  formatLinkForCaseStudy,
+} from "@/app/helpers/formatLink";
 import { usePathname, useRouter } from "next/navigation";
-const PortfolioList = ({data}:{data:Portfolio[]}) => {
-  const [portfolios, setPortfolios] = useState<Portfolio[]>([]);
+const PortfolioList = ({ data }: { data: Portfolio[] }) => {
+  // const [portfolios, setPortfolios] = useState<Portfolio[]>([]);
+    const [portfolios, setPortfolios] = useState<Portfolio[]>(data)
   const pathname = usePathname();
   // const CACHE_DURATION = 10 * 60 * 1000;
   // useEffect(() => {
@@ -50,17 +54,16 @@ const PortfolioList = ({data}:{data:Portfolio[]}) => {
   //   }
 
   // }, []);
-  
 
-  useEffect(() => {
-  if (data && data.length > 0) {
-    setPortfolios(data);
-  }
-}, [data]);
+  // useEffect(() => {
+  //   if (data && data.length > 0) {
+  //     setPortfolios(data);
+  //   }
+  // }, [data]);
 
   const [filter, setFilter] = useState("all");
 
-  const [originalPortfolio, setOriginalPortfolio] = useState<Portfolio[]>([])
+  const [originalPortfolio, setOriginalPortfolio] = useState<Portfolio[]>([]);
 
   const router = useRouter();
 
@@ -70,13 +73,12 @@ const PortfolioList = ({data}:{data:Portfolio[]}) => {
   //   }
   // }, [portfolios]);
 
-  const handleFiltering = (filter: string,link:string) => {
-
-    console.log(filter)
+  const handleFiltering = (filter: string, link: string) => {
+    console.log(filter);
 
     if (link) {
-    window.history.replaceState(null, "", `/portfolio${link}`);
-  }
+      window.history.replaceState(null, "", `/portfolio${link}`);
+    }
 
     setFilter(filter);
 
@@ -86,67 +88,63 @@ const PortfolioList = ({data}:{data:Portfolio[]}) => {
     } else {
       // Filter the original portfolio
       setPortfolios(
-        data.filter((portfolio:Portfolio) =>
-          portfolio.categories.some((category) => category.name === filter)
-        )
+        data.filter((portfolio: Portfolio) =>
+          portfolio.categories.some((category) => category.name === filter),
+        ),
       );
     }
   };
 
-  const [newFilterTags, setNewFilterTags] = useState<{name:string;link:string}[]>([])
-
+  const [newFilterTags, setNewFilterTags] = useState<
+    { name: string; link: string }[]
+  >([]);
 
   useEffect(() => {
     if (!pathname || newFilterTags.length === 0) return;
-    
+
     // remove "/portfolio"
     const currentPath = pathname.replace("/portfolio", "");
-    console.log(currentPath)
+    console.log(currentPath);
 
-  // if root portfolio page
-  if (currentPath === "") {
-    setFilter("all");
-    setPortfolios(data);
-    return;
-  }
+    // if root portfolio page
+    if (currentPath === "") {
+      setFilter("all");
+      setPortfolios(data);
+      return;
+    }
 
-
-  const matchedCategory = newFilterTags.find(
-    (item) => item.link === currentPath
-  );
-
-  if (matchedCategory) {
-    setFilter(matchedCategory.name);
-
-    setPortfolios(
-      data.filter((portfolio: Portfolio) =>
-        portfolio.categories.some(
-          (category) => category.name === matchedCategory.name
-        )
-      )
+    const matchedCategory = newFilterTags.find(
+      (item) => item.link === currentPath,
     );
-  }
-}, [pathname, newFilterTags, data]);
 
-useEffect(() => {
-  const allExistingCategories = data.flatMap((portfolio: Portfolio) =>
-    portfolio.categories.map((category) => ({
-      name: category.name,
-      link: category.link,
-    }))
-  );
+    if (matchedCategory) {
+      setFilter(matchedCategory.name);
 
-  // Remove duplicates using Map
-  const uniqueCategories = Array.from(
-    new Map(
-      allExistingCategories.map((item) => [item.name, item])
-    ).values()
-  );
+      setPortfolios(
+        data.filter((portfolio: Portfolio) =>
+          portfolio.categories.some(
+            (category) => category.name === matchedCategory.name,
+          ),
+        ),
+      );
+    }
+  }, [pathname, newFilterTags, data]);
 
-  setNewFilterTags(uniqueCategories);
-}, [data]);
+  useEffect(() => {
+    const allExistingCategories = data.flatMap((portfolio: Portfolio) =>
+      portfolio.categories.map((category) => ({
+        name: category.name,
+        link: category.link,
+      })),
+    );
 
+    // Remove duplicates using Map
+    const uniqueCategories = Array.from(
+      new Map(allExistingCategories.map((item) => [item.name, item])).values(),
+    );
 
+    setNewFilterTags(uniqueCategories);
+  }, [data]);
 
   return (
     <>
@@ -155,69 +153,106 @@ useEffect(() => {
           <span className="hidden">{process.env.NEXT_PUBLIC_BASE_URL}</span>
           <div className="portfolio pb-[50px] pt-[50px] lg:pb-[130px] lg:pt-[130px] ">
             {/* Filter Tabs */}
-            <div className="border-b  mb-[30px] md:mb-[50px] filterbtn no-scrollbar">
-
-              <div className="filter-tabs  flex space-x-4  w-full gap-[15px] md:gap-[30px] ">
-                <div className={`pb-1 md:pb-4 mb-[0px]  whitespace-nowrap divro ${filter === "all" ? "border-b-2 border-black text-black" : "text-gray1"
-                  }`}>
-                  <span onClick={() => handleFiltering("all","#")}>View All</span>
+            <div className="filterbtn  no-scrollbar mb-[30px] border-b md:mb-[50px]">
+              <div className="filter-tabs  flex w-full  gap-[15px] space-x-4 md:gap-[30px] ">
+                <div
+                  className={`divro mb-[0px] whitespace-nowrap  pb-1 md:pb-4 ${
+                    filter === "all"
+                      ? "border-b-2 border-black text-black"
+                      : "text-gray1"
+                  }`}
+                >
+                  <span onClick={() => handleFiltering("all", "#")}>
+                    View All
+                  </span>
                 </div>
                 {newFilterTags.map((item, index) => (
-                  <div key={index} className={`pb-1 md:pb-4 mb-[0px]  whitespace-nowrap divro ${filter === item.name ? "border-b-2 border-black text-black" : "text-gray1"
-                    }`}><span onClick={() => handleFiltering(item.name,item.link)}>{item.name}</span></div>
+                  <div
+                    key={index}
+                    className={`divro mb-[0px] whitespace-nowrap  pb-1 md:pb-4 ${
+                      filter === item.name
+                        ? "border-b-2 border-black text-black"
+                        : "text-gray1"
+                    }`}
+                  >
+                    <span onClick={() => handleFiltering(item.name, item.link)}>
+                      {item.name}
+                    </span>
+                  </div>
                 ))}
               </div>
             </div>
 
             {/* Portfolio Items */}
             <div className="flex flex-col items-center gap-8  lg:grid  lg:grid-cols-2 lg:gap-8 lg:gap-y-12 ">
-              {portfolios.length > 0 && portfolios.map((item, index) => (
-                <motion.div
-                  key={index}
-                  className="w-full"
-                  initial="hidden"
-                  whileInView="visible"
-                  viewport={{ once: true, amount: 0.1 }}
-                  variants={{
-                    hidden: { opacity: 0, y: 50 },
-                    visible: {
-                      opacity: 1,
-                      y: 0,
-                      transition: { duration: 1.3, ease: "easeOut" },
-                    },
-                  }}
-                >
-                  <div className="portfolio-card group relative col-span-1">
-                    <div className="card-img relative h-[300px] overflow-hidden rounded-md md:h-[500px] group">
-                      <Image
-                        src={item.bannerImage ?? item.coverImage}
-                        alt="image"
-                        className="h-full w-full object-cover"
-                        fill
-                      />
-                      <div className="absolute left-3 top-3 cursor-pointer rounded-3xl bg-gray1 px-4 py-2 duration-200 duration-300 ease-in-out ease-in-out   group-hover:z-[1] group-hover:-translate-x-[-3px] group-hover:bg-primary  group-hover:shadow-lg  md:left-5 md:top-5">
-                        <div className="uppercase text-white">
-                          <p className="text-font14 text-white">{item.industry}</p>
+              {portfolios.length > 0 &&
+                portfolios.map((item, index) => (
+                  <motion.div
+                    key={index}
+                    className="w-full"
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true, amount: 0.1 }}
+                    variants={{
+                      hidden: { opacity: 0, y: 50 },
+                      visible: {
+                        opacity: 1,
+                        y: 0,
+                        transition: { duration: 1.3, ease: "easeOut" },
+                      },
+                    }}
+                  >
+                    <div className="portfolio-card group relative col-span-1">
+                      <div className="card-img group relative h-[300px] overflow-hidden rounded-md md:h-[500px]">
+                        <Image
+                          src={item.bannerImage || item.coverImage}
+                          alt="image"
+                          className="h-full w-full object-cover"
+                          fill
+                        />
+                        <div className="absolute left-3 top-3 cursor-pointer rounded-3xl bg-gray1 px-4 py-2 duration-200 duration-300 ease-in-out ease-in-out   group-hover:z-[1] group-hover:-translate-x-[-3px] group-hover:bg-primary  group-hover:shadow-lg  md:left-5 md:top-5">
+                          <div className="uppercase text-white">
+                            <p className="text-font14 text-white">
+                              {item.industry}
+                            </p>
+                          </div>
+                        </div>
+                        <div className="absolute bottom-0 z-[1] mt-3 p-5 md:mt-4">
+                          <h3 className="text-30 mb-1 text-white duration-200  duration-200 duration-300  ease-in-out ease-in-out ease-in-out md:mb-2 ">
+                            {item.companyName}
+                          </h3>
+                          <div className="flex gap-1">
+                            {item?.channels && item.channels.length > 0 && item.channels.map((channel, index) => (
+                              <p
+                                key={index}
+                                className="text-19 text-gray1 text-white  duration-200 ease-in-out group-hover:-translate-x-[-3px] group-hover:text-primary"
+                              >
+                                {index == item.channels.length - 1
+                                  ? channel.channelName
+                                  : channel.channelName + ", "}
+                              </p>
+                            ))}
+                          </div>
+                          {!item?.channels && (
+                            <p className="text-19 text-gray1 text-white  duration-200 ease-in-out group-hover:-translate-x-[-3px] group-hover:text-primary">
+                              {item.channelsUsed}
+                            </p>
+                          )}
                         </div>
                       </div>
-                      <div className="absolute bottom-0 mt-3 md:mt-4 z-[1] p-5">
-                        <h3 className="text-30 mb-1 duration-200 ease-in-out  md:mb-2 text-white  duration-200 duration-300 ease-in-out ease-in-out ">
-                          {item.companyName}
-                        </h3>
-                        <div className="flex gap-1">
-                          {item?.channels?.map((channel, index) => (
-                            <p key={index} className="text-19 text-gray1 text-white  group-hover:-translate-x-[-3px] group-hover:text-primary duration-200 ease-in-out">{index == item.channels.length - 1 ? channel.channelName : channel.channelName + ", "}</p>
-                          ))}
-                        </div>
-                        {!item?.channels && <p className="text-19 text-gray1 text-white  group-hover:-translate-x-[-3px] group-hover:text-primary duration-200 ease-in-out">{item.channelsUsed}</p>}
-                      </div>
-                    </div>
 
-                    <Link href={item.section == "case study" || item.section == "case study new" ? `/case-study/${formatLinkForCaseStudy(item.companyName)}` : `/portfolio/${formatLinkForPortfolio(item.companyName)}`}
-                      className="absolute top-0 z-[1] h-full w-full" ></Link>
-                  </div>
-                </motion.div>
-              ))}
+                      <Link
+                        href={
+                          item.section == "case study" ||
+                          item.section == "case study new"
+                            ? `/case-study/${formatLinkForCaseStudy(item.companyName)}`
+                            : `/portfolio/${formatLinkForPortfolio(item.companyName)}`
+                        }
+                        className="absolute top-0 z-[1] h-full w-full"
+                      ></Link>
+                    </div>
+                  </motion.div>
+                ))}
             </div>
           </div>
         </div>
