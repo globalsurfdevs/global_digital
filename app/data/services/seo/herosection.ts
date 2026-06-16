0df65a48bdd4eda61c394fadc74530a2cf6eb068
rahees <raheez1995india@gmail.com>
2026-06-16 13:29:37 +0530

    SEO agency in dubai page seo changes done

--- app/data/services/seo/herosection.ts
+++ app/data/services/seo/herosection.ts
@@ -5,16 +5,17 @@ export const BannerSection = [
     id: 1,
     navigation: [
       { label: "Home", url: "/" },
-      { label: "Digital Marketing",url: "/digital-marketing-services" },
-      { label: "SEO",url: "" },
+      { label: "Digital Marketing", url: "/digital-marketing-services" },
+      { label: "SEO", url: "" },
     ],
-    title: "SEO Services Agency in Dubai",
-    subtitle:"Enhancing Search Engine Visibility with Advanced Optimization Strategies to Boost Traffic and Rankings.",
+    title: "SEO Agency in Dubai",
+    subtitle:
+      "Enhancing Search Engine Visibility with Advanced Optimization Strategies to Boost Traffic and Rankings.",
     image: assets.seo_banner,
     sub: [
       {
         stitle: "OUR APPROACH",
-        buttonTitle :"Start Your Project",
+        buttonTitle: "Start Your Project",
         desc: "Our data-driven SEO strategies attract high-quality leads by optimizing your website to rank higher on Google. We target decision-makers and buyers, turning cold leads into warm prospects. Enhance your brand’s trust and credibility while driving growth with search engine optimization that delivers results. ",
       },
     ],
