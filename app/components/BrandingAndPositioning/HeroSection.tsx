9c72dfc9720262ca48073c1bd98c2fb054b2188d
rahees <raheez1995india@gmail.com>
2026-08-02 11:26:09 +0530

    button space fix on small device

--- app/components/BrandingAndPositioning/HeroSection.tsx
+++ app/components/BrandingAndPositioning/HeroSection.tsx
@@ -11,7 +11,7 @@ const HeroSection = () => {
       <div className="container relative z-10 h-full flex flex-col justify-center">
         <div>
           <h1 className="title-70 max-w-[16ch] mb-3 xl:mb-5 tracking-[-0.025em]">{heroData.title}</h1>
-          <p className="text-25 leading-[1.4] text-77787B max-w-[53ch]">{heroData.description}</p>
+          <p className="text-25 leading-[1.4] text-77787B 2xl:max-w-[45ch] 3xl:max-w-[53ch]">{heroData.description}</p>
           <div className="flex flex-wrap items-center gap-4 mt-4 xl:mt-8 xxl:mt-[60px]">
             <Button variant="primary" href="/contact">
               Let&apos;s discuss your project
