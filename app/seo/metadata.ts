329b43b198a730a1eab77380f8994a5693baf8de
Muhammed fayiz | Globalsurf <muhammed.fayiz@globalsurf.ae>
2026-09-26 20:39:12 +0530

    feat: move SEO elements to separate seo folder

---
+++ app/seo/metadata.ts
@@ -0,0 +1,12 @@
+import { Metadata } from "next";
+
+export const siteMetadata: Metadata = {
+  title: {
+    default: "GS Digital",
+    template: "%s | GS Digital", // page title will be: "Page Title | Global Surf Digital"
+  },
+  description: "Full-service digital marketing agency in Dubai.",
+  other: {
+    "linkedin-domain-verification": "cd0771a6-efa9-4299-a6d6-35ba3450f6e0",
+  },
+};
---
+++ app/seo/schemas.ts
@@ -0,0 +1,108 @@
+
+export const organizationSchema = {
+  "@context": "https://schema.org",
+  "@type": "Organization",
+  name: "GS Digital",
+  legalName: "Global Surf Digital Media L.L.C",
+  url: "https://www.globalsurf.ae/",
+  logo: "https://www.globalsurf.ae/gs-digital-logo.svg",
+  description:
+    "GS Digital Media is a premium full-service digital marketing agency in Dubai, specializing in SEO, performance marketing, social media marketing, and web design and development.",
+  telephone: "+971-4-582-1133",
+  email: "hello@globalsurf.ae",
+  sameAs: [
+    "https://www.facebook.com/globalsurf.digital/",
+    "https://www.instagram.com/globalsurf.digital/",
+    "https://x.com/GlobalSurf_D",
+    "https://www.linkedin.com/company/globalsurfdigital",
+    "https://www.tiktok.com/@globalsurf.digital",
+  ],
+  address: {
+    "@type": "PostalAddress",
+    streetAddress: "P.O. Box 13653, 901 - SIT Tower, Dubai Silicon Oasis",
+    addressLocality: "Dubai",
+    addressCountry: {
+      "@type": "Country",
+      name: "AE",
+    },
+  },
+};
+
+//  Old Organization Schema
+// const organizationSchema = {
+//   "@context": "https://schema.org",
+//   "@type": "Organization",
+//   name: "GS Digital",
+//   url: "https://www.globalsurf.ae/",
+//   logo: "https://www.globalsurf.ae/gs-digital-logo.svg",
+//   description:
+//     "GS Digital Media is a premium full-service digital marketing agency in Dubai, specializing in SEO, performance marketing, social media marketing, and web design and development.",
+//   slogan: "Master Every Pixel of Your Digital Presence",
+//   foundingDate: "2013",
+//   address: {
+//     "@type": "PostalAddress",
+//     streetAddress: "P.O.Box 13653, 901 - SIT Tower, DSO",
+//     addressLocality: "DSO",
+//     addressRegion: "Dubai",
+//     postalCode: "13653",
+//     addressCountry: "AE",
+//   },
+//       "@type": "Country",
+//       openingHoursSpecification: [
+//         "Monday – Friday, 8:00 – 18:00",
+//       ],
+//       hasmap:
+//       geo: {
+//         "@type": "GeoCoordinates",
+//         latitude: "25.1177885",
+//       },
+//     },
+//   contactPoint: {
+//     "@type": "ContactPoint",
+//     contactType: "customer service",
+//     areaServed: "AE",
+//   },
+//   aggregateRating: {
+//     ratingValue: "5",
+//     reviewCount: "6",
+//   },
+//   review: [
+//       "@type": "Review",
+//       author: {
+//         name: "Karim El Shennawy",
+//       },
+//         "Global Surf proved to be talented group that delivered their project in excellent manner. They are responsive, and we trusted them day by day with more tasks and they continue to prove their capabilities.",
+//       "@type": "Review",
+//         "@type": "Person",
+//         name: "Alissar Nasrallah",
+//       description:
+//     },
+//     {
+//       "@type": "Review",
+//       author: {
+//         "@type": "Person",
+//         name: "Hesham Abdeen",
+//       },
+//       description:
+//         "Because of the way that Global Surf encourages collaboration, working with the team has been a pleasure. Their staff welcomes our input and fosters open communication, which has led to a website that reflects our brand and serves our particular demands. Global Surf offers a plethora of knowledge and creativity.",
+//     },
+//     {
+//       "@type": "Review",
+//       author: {
+//         "@type": "Person",
+//         name: "Omar M. Bin Dhaher Almheiri",
+//       },
+//       description:
+//         "We have vary good relation and experience with your professional company Not to mention the extra care we get from your team We like this relationship to continue for the success of both of us",
+//     },
+//   ],
+//   sameAs: [
+//     "https://www.facebook.com/globalsurf.digital",
+//     "https://www.instagram.com/globalsurf.digital/",
+//     "https://x.com/GlobalSurf_D",
+//     "https://www.linkedin.com/company/globalsurfdigital",
+//     "https://www.tiktok.com/@globalsurf.digital",
+//   ],
+// };
+
+
